cb3b4dc3ba5cffad797da9fa4220586a9cc65683
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-21 21:17:10 +0100

    feat: created socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { Socials } from "@/components/HeroSection/Socials";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Socials/>
     </>
   );
 }
---
+++ src/components/HeroSection/Socials.tsx
@@ -0,0 +1,44 @@
+import { Github, Linkedin, LucideIcon, Mail, Phone } from "lucide-react";
+import Link from "next/link";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+interface SocialInterface {
+  name: string;
+  icon: LucideIcon;
+  href: string;
+}
+
+export const Socials = () => {
+  return (
+    <div className="flex gap-4">
+      {socialContacts.map(social => <Link key={social.name} href={social.href} > <social.icon strokeWidth={2}/> </Link>)}
+      <Popover >
+        <PopoverTrigger asChild>
+            <Phone className="cursor-pointer "/>
+        </PopoverTrigger>
+        <PopoverContent className="flex justify-center w-fit rounded-full" >
+            <Link href="tel:+36205748584">{"+36/20 - 574 - 8584"}</Link>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+const socialContacts: SocialInterface[] = [
+  {
+    name: "LinkedIn",
+    icon: Linkedin,
+    href: "https://www.linkedin.com/in/gergoszabo87/",
+  },
+  {
+    name: "GitHub",
+    icon: Github,
+    href: "https://github.com/geriszabo",
+  },
+  {
+    name: "Email",
+    icon: Mail,
+    href: "mailto:gergoszabodev@gmail.com",
+  }
+
+];
