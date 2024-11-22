4de9cb97a47ca2b2313b9890aa932243c9f0cb7b
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-22 14:09:48 +0100

    refactor: refactored typography comp to accept classnames

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,17 +1,30 @@
-import { useTranslations } from "next-intl"
+import { useTranslations } from "next-intl";
+import { HTMLProps } from "react";
 
-interface TypographyProps{
-    variant: "title" | "subtitle" | "paragraph",
-    namespacePath: string
-    text: string
+export interface TypographyProps {
+  variant: "title" | "subtitle" | "paragraph";
+  namespacePath: string;
+  text: string;
+  className?: HTMLProps<HTMLElement>["className"];
 }
 
-export const Typography = ({namespacePath, text, variant}: TypographyProps) => {
-    const t = useTranslations(namespacePath)
-    if(variant === "subtitle" || variant === "title") {
-        return <h1 className={`${variant === "title" ? "text-5xl" : "text-2xl"}`} >{t(text)}</h1>
-    }
-  return (
-    <p>{t(text)}</p>
-  )
-}
+export const Typography = ({
+  namespacePath,
+  text,
+  variant,
+  className,
+}: TypographyProps) => {
+  const t = useTranslations(namespacePath);
+  if (variant === "subtitle" || variant === "title") {
+    return (
+      <h1
+        className={`${
+          variant === "title" ? "text-5xl" : "text-2xl"
+        } ${className}`}
+      >
+        {t(text)}
+      </h1>
+    );
+  }
+  return <p className={`${className}`}>{t(text)}</p>;
+};
