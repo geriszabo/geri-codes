63633746080ba841907c0d59ae1594106ff853dc
Jetigeri <szabo.gergo87j@gmail.com>
2024-11-25 13:09:16 +0100

    chore: added translations to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,31 @@
-import { Typography } from "@/components/Typography";
+import { Typography, TypographyPropCollection } from "@/components/Typography";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function NotFound() {
+  const typographyTitleProps: TypographyPropCollection = {
+    namespacePath: "notFound",
+    variant: "title",
+  };
+  const typographyParagraphProps: TypographyPropCollection = {
+    namespacePath: "notFound",
+    variant: "paragraph",
+  };
   return (
     <article>
       <div className="min-h-full px-4 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <section className="sm:flex">
-          <Typography namespacePath="" text="404" variant="title" />
+          <Typography {...typographyTitleProps} text="code" />
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-              <Typography
-                namespacePath=""
-                text="you're not supposed to be here"
-                variant="title"
-              />
-              <Typography
-                namespacePath=""
-                text="let's take you home"
-                variant="paragraph"
-              />
+              <Typography {...typographyTitleProps} text="title" />
+              <Typography text="subtitle" {...typographyParagraphProps} />
             </div>
             <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
               <Button variant="default" size="lg" asChild>
-                <Link href="/">🏠 click here</Link>
+                <Link href="/">
+                  {<Typography text="button" {...typographyParagraphProps} />}
+                </Link>
               </Button>
             </div>
           </div>
